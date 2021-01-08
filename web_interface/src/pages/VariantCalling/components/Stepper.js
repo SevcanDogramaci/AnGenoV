e26ab5429d75062b22fa457ac4ab3a3b9837f42c
SevcanDogramaci <sevcan.dog@gmail.com>
2021-01-08 15:17:26 +0300

    Stepper components updated

--- web_interface/src/pages/VariantCalling/components/Stepper.js
+++ web_interface/src/pages/VariantCalling/components/Stepper.js
@@ -4,11 +4,25 @@ const Stepper = (props) => {
 
     return (
         
-        <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between", width:"50%"}}>
+        <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around", 
+                    alignItems: "center",  width:"50%"}}>
             {
-                props.steps.map(stepName => {
-                    return <StepButton isActive={props.activeStep === stepName} onClick={props.onStepClick} 
-                                       name={stepName} to="/calling"/>
+                props.steps.map((stepName, id) => {
+                    return  <>
+
+                            <StepButton isActive={props.activeStep === stepName} onClick={props.onStepClick} 
+                                    name={stepName} to="/calling"/>
+
+                            {(id != (props.steps.length-1)) &&
+                            <hr style={{
+                                        color: "#d4d4d4",
+                                        backgroundColor: "#d4d4d4",
+                                        width: "20%",
+                                        height:1,
+                                        borderWidth: 0
+                                    }} />}
+                            </>
+                            
                 })
             }
         </div>
