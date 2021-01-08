67c1a137adf658e6b108c385a6115a23812a10b6
SevcanDogramaci <sevcan.dog@gmail.com>
2021-01-08 12:40:25 +0300

    Refactor

--- web_interface/src/layouts/DefaultLayout.js
+++ web_interface/src/layouts/DefaultLayout.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import NavBar from "../components/NavBar"
-import CallingPage from "../pages/Calling/CallingPage"
-import AnnotationPage from "../pages/Annotation/AnnotationPage"
+import AnnotationPage from "../pages/VariantAnnotation/VariantAnnotationPage"
+import VariantCallingPage from '../pages/VariantCalling/VariantCallingPage';
 
 const DefaultLayout = (props) => {
 
   return (
     <>
         <NavBar/>
-        <Route exact path="/" render={props => <CallingPage {...props} />}/>    
-        <Route path="/calling" render={props => <CallingPage {...props} />} />
+        <Route exact path="/" render={props => <VariantCallingPage {...props} />}/>    
+        <Route path="/calling" render={props => <VariantCallingPage {...props} />} />
         <Route path="/annotation" render={props => <AnnotationPage {...props} />} />
     </>
   );
