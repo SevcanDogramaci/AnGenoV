bbb98bbe89add5bef069fccb78c1201f034b61ba
SevcanDogramaci <sevcan.dog@gmail.com>
2021-03-26 21:56:09 +0300

    Function updates are made and outputs are saved to temporary folder

--- server/merge.py
+++ server/merge.py
@@ -1,6 +1,34 @@
 import subprocess
+import os
+import shutil
 
 def runSurvivor(file_name):
-    argsSurvivor = ("SURVIVOR", "merge", file_name, "1000", "2", "1", "1", "0", "30", "sample_merged.vcf")
+    fileList = file_name.split(".vcf")
+    responseMessages = []
+    files = []
+    dir_path = os.getcwd() + "/temp/merge/"
+    try:
+        shutil.rmtree(dir_path, ignore_errors=True)
+        os.makedirs("temp/merge")
+    except OSError as e:
+        print("Error: %s : %s" % (dir_path, e.strerror))
+
+    f = open(os.getcwd()+"/temp/merge/mergefile", "w")
+    for i in range (0,len(fileList)-1):
+        f.write(fileList[i] + ".vcf" + "\n")
+    f.close()
+
+    with open("temp/merge/mergefile", "r") as f:
+        data = f.read()
+        with open("mergefile", "w") as w:
+            w.write(data[:-1])
+
+
+    argsSurvivor = ("SURVIVOR", "merge", "mergefile", "1000", "2", "1", "1", "0", "30", "temp/merge/merged.vcf")
     popen=subprocess.Popen(argsSurvivor)
     popen.wait()
+
+    files.append(os.getcwd() + "/temp/merge")
+    responseMessages.append("Merging is finished successfully\n")
+
+    return (responseMessages, files)
--- server/sv_calling.py
+++ server/sv_calling.py
@@ -1,21 +1,64 @@
 import subprocess 
+import os
+import shlex
+import sys
+import shutil
+from pathlib import Path
 
-def runDelly(referenceFile, alignedFile):
-    logfile = open("/home/mert/Desktop/AnGenoV/web_interface/src/pages/VariantCalling/Calling/logfile.txt" , "w")
-    argsDelly = ("delly", "call", "-o", "outputDelly.bcf", "-g", referenceFile, alignedFile)
-    popen=subprocess.Popen(argsDelly, stdout=logfile)
-    popen.wait()
+out=""
+def runSVCallerTool (referenceFile, alignedFile, toolName, allArgs):
+    import os
+    
+    print(toolName)
+    for arg in allArgs:
+        popen=subprocess.Popen("x-terminal-emulator -e \"echo " + toolName + " is running...;echo;" + arg + "\"", shell=True, stdout=subprocess.PIPE)
+        popen=subprocess.Popen(arg, stdout=subprocess.PIPE)
+        popen.wait()
 
-def runTardis(referenceFile, alignedFile):
-    logfile = open("/home/mert/Desktop/AnGenoV/web_interface/src/pages/VariantCalling/Calling/logfile.txt" , "w")
-    argsTardis = ("tardis", "--no-interdup", "--no-mei" , "-i" , alignedFile, "--ref", referenceFile, "--sonic", "human_g1k_v37.sonic", "--out", "outputTardis")
-    popen=subprocess.Popen(argsTardis, stdout=logfile)
-    popen.wait()
+    toolName = toolName.lower()
+    return os.getcwd() + "/temp/ToolsOutputs/"  + toolName
 
-def runManta(referenceFile, alignedFile):
-    logfile = open("/home/mert/Desktop/AnGenoV/web_interface/src/pages/VariantCalling/Calling/logfile.txt" , "w")
-    argsConfManta = ("configManta.py" , "--bam", alignedFile, "--referenceFasta", referenceFile, "--runDir", "outputManta")
-    argsRunManta = ("outputManta/runWorkflow.py")
-    popen=subprocess.Popen(argsConfManta, stdout=logfile)
-    popen.wait()
-    popen=subprocess.Popen(argsRunManta, stdout=logfile)
+def runSelectedTools(reference_file, aligned_file, selectedTools):
+    import json
+
+    print("Inputs >> ", reference_file, aligned_file, selectedTools)
+    responseMessages = []
+    files = []
+    dir_path = os.getcwd() + "/temp/ToolsOutputs/"
+    print("checkpoint1")
+    try:
+        shutil.rmtree(dir_path, ignore_errors=True)
+        print("checkpoint1.1")
+        os.makedirs("temp/ToolsOutputs")
+    except OSError as e:
+        print("Error: %s : %s" % (dir_path, e.strerror))
+   
+    with open(os.getcwd()+"/../config/toolConfigs.json") as json_file:
+        data = json.load(json_file)
+        for tool in data["tools"].values():
+            print(tool["name"])
+            if tool["name"] in selectedTools:
+                print(tool["name"], " selected")
+                allArgs = []
+                
+                for param in tool["lastUsedParams"].split(","):
+                    outputFile = "temp/ToolsOutputs/" + tool["name"].lower() + "/" + tool["outputName"]
+                    args = param.strip().replace("${referenceFile}", reference_file).replace("${alignedFile}", aligned_file).replace("${outputName}", outputFile)
+                    allArgs.append(args)
+                
+                print(allArgs)
+                print("checkpoint2")
+                try:
+                    print("checkpoint2.1")
+                    os.makedirs("temp/ToolsOutputs/"+tool["name"].lower())
+                except OSError as e:
+                    print("Error: %s : %s" % (dir_path, e.strerror))
+                outputFileForResponse = runSVCallerTool(reference_file, aligned_file, tool["name"], allArgs)
+                responseMessages.append( tool["name"] + " is finished successfully\n")
+                files.append(outputFileForResponse)
+
+    return (responseMessages, files)
+    
+
+
+
