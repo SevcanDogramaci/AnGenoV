86c9ea769231bdba2ef058d763f250243306e13c
SevcanDogramaci <sevcan.dog@gmail.com>
2021-03-26 22:10:24 +0300

    Merge function and UI are updated

--- web_interface/src/pages/VariantCalling/MergeSV/MergePage.js
+++ web_interface/src/pages/VariantCalling/MergeSV/MergePage.js
@@ -1,29 +1,46 @@
 import React, { useState } from 'react';
 import { 
     Button, 
-    Menu, 
-    MenuItem, 
-    Popover, 
     Tooltip, 
     Position, 
     Icon,
-    PopoverPosition, 
     FileInput
 } from "@blueprintjs/core";
 
 import Service from '../../../services/Service';
 
-const MergePage = () => {
-    const [VCFfile, setVCFfile] = useState(undefined);
-    const [mergeTool, setMergeTool] = useState("Truvari");
+const { ipcRenderer } = require('electron');
 
-    const updateMergeTool = (mergeTool) => { setMergeTool(mergeTool);}
+const MergePage = () => {
+
+    const [filePaths, setFilePaths] = useState("");
+    const [responseMessage, setResponseMessage] = useState(undefined);
+    const [running, setRunning] = useState(false);
+
+    const saveFilesToSelectedDirectory = (e) => {
+        ipcRenderer.send('show-open-dialog', {"files": responseMessage.fileName});  
+    }
+
+    const viewFile = (e) => { ipcRenderer.send('view-file', responseMessage.fileName[0]); }
 
     const handleFileChange = (event) => {
+        let paths = "";
 
-        if (event.target.files[0]) {
-            setVCFfile(event.target.files[0]);
+        for(let i=0; i<event.target.files.length; i++){
+            paths += event.target.files[i].path + "\n";       
         }
+        setFilePaths(paths);
+    }
+
+    const runMergeTool = (event) => {
+        setRunning(true);
+        Service.runSurvivor(filePaths)
+            .then(
+                response => {
+                    setRunning(false);
+                    setResponseMessage(response);
+                }
+            )
     }
 
     return (
@@ -31,29 +48,21 @@ const MergePage = () => {
                         marginTop:"2.5%", justifyContent:"center"}}>
             <h3 className="bp3-heading"> Merge SV</h3>
             <p style={{marginTop:"2%"}}>Load your SVs in VCF format</p>
+
             <div style={{display:"flex", flexDirection:"row",  alignItems:"center"}}> 
-                    <FileInput buttonText="Browse" text={VCFfile == undefined ? "Choose file" : VCFfile.name}  style={{margin:"2%"}}
-                                onInputChange={handleFileChange}/>
-                <Tooltip content="At least 2 VCF files or 1 path file" position={Position.RIGHT} intent="warning">
+                <FileInput buttonText="Browse" style={{margin:"2%"}} text={filePaths}
+                    onInputChange={handleFileChange} inputProps={{multiple: true}}
+                    style={{width:"600px"}}/>
+                <Tooltip content="At least 2 VCF files" position={Position.RIGHT} intent="warning">
                     <Icon icon="info-sign" intent="warning"/>
-                </Tooltip> </div>
+                </Tooltip> 
+            </div>
 
-            <p style={{marginTop:"7%"}}>Select an algorithm</p>
+            <Button text="Merge" disabled={running} onClick={runMergeTool} style={{marginTop:"4%"}}/>
 
-            <Popover content={
-                <Menu>
-                    <MenuItem text="Truvari" onClick={e => updateMergeTool("Truvari")}/>
-                    <MenuItem text="SURVIVOR" onClick={e => updateMergeTool("SURVIVOR")}/>
-                </Menu>} position={PopoverPosition.BOTTOM}>
-                <Button rightIcon="caret-down" text={mergeTool}/>
-            </Popover>
-
-            <Button text="Merge" onClick={e => Service.runSurvivor(VCFfile.path).then(res => console.log(res))}
-                     style={{marginTop:"4%"}}/>
-            <div style={{display:"flex", flexDirection:"row", marginTop:"2%"}}>
-                <Button text="Save Output"/>
-                &nbsp;&nbsp;&nbsp;
-                <Button text="View Output"/>
+            <div style={{display:"flex", flexDirection:"row", marginTop:"2%", width: "40%", justifyContent: "space-evenly"}}>
+                <Button text="Save Output" onClick={saveFilesToSelectedDirectory}/>
+                <Button text="View Output" onClick={viewFile}/>
             </div>
         </div>
     );
