0580f53917ec0ac18aea6f484b3c772508cc9104
SevcanDogramaci <sevcan.dog@gmail.com>
2021-03-26 22:06:58 +0300

    Saving and viewing output files and showing output results are added

--- web_interface/src/pages/VariantCalling/Calling/Outputs.js
+++ web_interface/src/pages/VariantCalling/Calling/Outputs.js
@@ -1,77 +1,83 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useContext }  from 'react';
 import { 
-    Checkbox, 
     Button, 
-    ControlGroup, 
     Pre, 
-    Collapse 
 } from "@blueprintjs/core";
+import RunningText from './components/RunningText';
+import { CallingContext } from './CallingContext';
+import OutputFile from './components/OutputFile';
 
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+const { Set } = require('immutable');
 
 const Outputs = (props) => {
 
-    const [logs, setLogs] = useState("");
-    const [logsOpen, setLogsOpen] = useState(true);
+    const context = useContext(CallingContext);
+    const [checkedOutputFiles, setCheckedOutputFiles] = useState(Set());
 
-    useEffect(() => {
+    const saveFilesToSelectedDirectory = () => {
 
-        ipcRenderer.on('log-file-reply', (event, arg) => {
-            if (arg != undefined) {
-                setLogs(arg["data"]);
+        let fileNames = checkedOutputFiles.toArray();
+        console.log(fileNames);
+        ipcRenderer.send('show-open-dialog', {"files": fileNames});  
+    }
 
-                if (!props.isRunning)
-                    setLogsOpen(false);
-            }
-        });
-    }, []);
+    const viewFile = (fileName) => {
+        console.log(fileName);
+        ipcRenderer.send('view-file', fileName);
+    }
+
+    const handleOutputFileChanged = (filePath, checked) => {
+        console.log("handleOutputFileChanged !", filePath, checked);
+        if(checked) {
+            setCheckedOutputFiles(checkedOutputFiles.add(filePath));
+        }
+    }
 
     return (
        
         <div style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"flex-start", height:"75vh"}}>
-         {console.log("Algorithms >>", props.algorithms)}
+
+         {console.log("Algorithms >>", context.checkedCallers)}
+
             <h3 className="bp3-heading">Outputs</h3>
 
             {/* Show nothing when no tools running */}
-            {props.isRunning === undefined ? <p>No outputs</p> :
+            {context.running === undefined ? <p>No outputs</p> :
 
             /* Show logs while running */
             <>
-                {props.isRunning === false &&
-                <Button onClick={e => setLogsOpen(!logsOpen)}>{logsOpen ? "Hide" : "Show"} build logs</Button>}
-
-                <div style={{overflowY:"scroll",  alignSelf: 'stretch'}}>
-                    <Collapse isOpen={props.isRunning ? true : logsOpen} style={{height: 100}}>
-                        <Pre> {logs} </Pre>
-                    </Collapse>
+                <div style={{display:"flex",alignSelf: 'stretch', flexDirection: "column", justifyContent: "center"}}>
+                    <Pre style={{display: "flex", flexDirection: "column"}}>
+                        {<RunningText stop={context.running === false}/>}
+                        {context.running === false && context.responseMessages.message.map((element, id) => <div>
+                            {element}
+                        </div>)}
+                    </Pre>
                 </div>
                 
                 {/* Show files when running finishes */}
-                {props.isRunning === false &&
-                <>  
+                {context.running === false &&
+                <>     
                     <div style={{width:"50%"}}>
-                        { props.algorithms.map((algorithm, id) =>  
-                            <div style={{display:"flex", flexDirection:"row", marginTop:"2%",
-                                        justifyContent:"space-between", alignItems:"center"}}>
-                                <p key={id}>{algorithm.toLowerCase().concat("Output.vcf")}</p>
-                                <ControlGroup style={{display:"flex", alignItems:"center"}}>
-                                    <Checkbox></Checkbox>
-                                    <Button>View</Button>
-                                </ControlGroup>
-                            </div>)}
+                    {console.log("OutputFile >>>", context.responseMessages, context.responseMessages.fileName)}
+                        {
+
+                            context.responseMessages.fileName.map((fileName, id) =>
+                            <OutputFile 
+                                onOutputFileChecked={handleOutputFileChanged} 
+                                filePath={fileName} />
+                        )}
                     </div>
 
                     <div style={{width: "50%", marginTop: "1%"}}>
-                        <Button fill={true} onClick={e => console.log("Saving...")}>
+                        <Button fill={true} onClick={e => {
+                            console.log("Saving..."); 
+                            saveFilesToSelectedDirectory();}}>
                             Save
                         </Button>
                     </div>
 
-                    <div style={{width: "50%", marginTop: "1%"}}>
-                        <Button fill={true} onClick={e => console.log("Saving...")}>
-                            Send to Merge Step
-                        </Button>
-                    </div> 
                 </>}
             </>}
         </div>
