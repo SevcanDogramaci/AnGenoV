b54443a1a7b38a3acaa2fcbb0711b77777a8c796
SevcanDogramaci <sevcan.dog@gmail.com>
2021-05-08 01:35:39 +0300

    Annotation context added

--- web_interface/src/layouts/DefaultLayout.js
+++ web_interface/src/layouts/DefaultLayout.js
@@ -6,23 +6,24 @@ import VariantCallingPage from '../pages/VariantCalling/VariantCallingPage';
 import HomePage from '../pages/Home/HomePage';
 import NavBar from '../components/NavBar';
 
-const DefaultLayout = (props) => {
-	return (
-		<HashRouter>
-			<NavBar />
+import AnnotationContextProvider from '../pages/VariantAnnotation/AnnotationContext';
 
-			<Route path="/home" render={(props) => <HomePage {...props} />} />
-			<Route
-				path="/calling"
-				render={(props) => <VariantCallingPage {...props} />}
-			/>
-			<Route
-				path="/annotation"
-				render={(props) => <AnnotationPage {...props} />}
-			/>
-			<Route exact path="/" render={(props) => <HomePage {...props} />} />
-		</HashRouter>
-	);
-};
+const DefaultLayout = (props) => (
+	<HashRouter>
+		<NavBar />
+
+		<Route path="/home" render={(props) => <HomePage {...props} />} />
+		<Route path="/calling" render={(props) => <VariantCallingPage {...props} />} />
+		<Route
+			path="/annotation"
+			render={(props) => (
+				<AnnotationContextProvider>
+					<AnnotationPage {...props} />
+				</AnnotationContextProvider>
+			)}
+		/>
+		<Route exact path="/" render={(props) => <HomePage {...props} />} />
+	</HashRouter>
+);
 
 export default DefaultLayout;
