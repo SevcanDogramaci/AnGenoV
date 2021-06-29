1b0e0e8d41d1875fa475d8ae3b558366b9c2945d
mert <mert.dogan@ogr.gidatarim.edu.tr>
2021-06-29 18:00:08 +0300

    Merge Error Handling

--- server/merge.py
+++ server/merge.py
@@ -3,6 +3,9 @@ import os
 import shutil
 
 def runSurvivor(fileList):
+    success=True
+    errorMsg=""
+    toolOut=""
     fileList = fileList[0].split(",")
     responseMessages = []
     files = []
@@ -30,12 +33,30 @@ def runSurvivor(fileList):
     with open(os.getcwd()+"/../config/toolConfigs.json") as json_file:
         data = json.load(json_file)
         outputFile = "temp/merge/" + data["tools"]["SURVIVOR"]["outputName"] + ".vcf"
-        args = data["tools"]["SURVIVOR"]["lastUsedParams"].strip().replace("${outputName}", outputFile).split(" ")
-
-    popen=subprocess.Popen(args)
-    popen.wait()
+        args = data["tools"]["SURVIVOR"]["lastUsedParams"].strip().replace("${outputName}", outputFile)
+    
+    args = "bash -c 'set -o pipefail;" + args + " 2>&1 | tee log.txt'"
+    popen=subprocess.run(args, shell=True, executable='/bin/bash')
+    returncode=popen.returncode
+    f = open("log.txt", "r")
+    if returncode != 0 :
+        success = False
+        errorMsg += f.read()
+        f.close()
+    else:
+        success = True
+        toolOut += f.read()
+        f.close()
     os.remove("temp/merge/mergefile")
-    files.append(os.getcwd() + "/temp/merge")
-    responseMessages.append("Merging is finished successfully\n")
+    if success:
+        responseMessages.append("Merging is finished successfully\n")
+        with open(os.getcwd() + "/temp/merge/consoleLogs.txt", 'w') as f:
+            f.write(toolOut)
+            f.close()
+        toolOut=""
+        files.append(os.getcwd() + "/temp/merge")
+    else:  
+        responseMessages.append( "*---" + "An error occured:\n " + errorMsg)
+        errorMsg=""
 
     return (responseMessages, files)
